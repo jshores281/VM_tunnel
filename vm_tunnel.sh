3f6c29c1064b48fbf0e00ec185da44140b6c26c7
jshores281 <52839097+jshores281@users.noreply.github.com>
2022-01-07 01:53:24 -0600

    system and user agnostic

--- vm_tunnel.sh
+++ vm_tunnel.sh
@@ -3,8 +3,8 @@
 
 
 echo "fusing vm_tunnel folder from windows 10 to ubuntu20"
-/usr/bin/vmhgfs-fuse .host:/ /home/morbiter1/Documents/vm_tunnel -o subtype=vmhgfs-fuse,nonempty
-cd /home/morbiter1/Documents
+/usr/bin/vmhgfs-fuse .host:/ /home/$USER/Documents/vm_tunnel -o subtype=vmhgfs-fuse,nonempty
+cd /home/$USER/Documents
 
 # write if in dir logic to confirm fuse completion
 echo "fusing completed?"
